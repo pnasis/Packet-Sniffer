6074e9274ad7b0b86781715a02a3dc9f14c5ccfe
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2023-07-21 00:34:42 +0300

    Update sniffer.c

--- sniffer.c
+++ sniffer.c
@@ -6,6 +6,7 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ether.h>
+#include <unistd.h>
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 
@@ -16,34 +17,91 @@ int main(int argc, char *argv[])
     struct bpf_program fp;
     bpf_u_int32 net, mask;
 
-    if (argc < 2) {
-        printf("Usage: %s [interface]\n", argv[0]);
+    char *interface = NULL;
+    char filter_exp[512] = ""; // Buffer to store the filter expression
+
+    // Use getopt to parse the command-line arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "p:s:d:sp:dp:")) != -1) {
+        switch (opt) {
+            case 'p':
+                strcat(filter_exp, optarg);
+                strcat(filter_exp, " and ");
+                break;
+            case 's':
+                strcat(filter_exp, "src host ");
+                strcat(filter_exp, optarg);
+                strcat(filter_exp, " and ");
+                break;
+            case 'd':
+                strcat(filter_exp, "dst host ");
+                strcat(filter_exp, optarg);
+                strcat(filter_exp, " and ");
+                break;
+            case 'sp':
+                strcat(filter_exp, "src port ");
+                strcat(filter_exp, optarg);
+                strcat(filter_exp, " and ");
+                break;
+            case 'dp':
+                strcat(filter_exp, "dst port ");
+                strcat(filter_exp, optarg);
+                strcat(filter_exp, " and ");
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [options] [interface]\n", argv[0]);
+                fprintf(stderr, "Options:\n");
+                fprintf(stderr, "  -p protocol\tSpecify desired protocol (e.g., tcp, udp, icmp, arp)\n");
+                fprintf(stderr, "  -s source_ip\tSpecify source IP address\n");
+                fprintf(stderr, "  -d dest_ip\tSpecify destination IP address\n");
+                fprintf(stderr, "  -sp source_port\tSpecify source port\n");
+                fprintf(stderr, "  -dp dest_port\tSpecify destination port\n");
+                return 1;
+        }
+    }
+
+    // After parsing the options, there should be the interface argument
+    if (optind == argc) {
+        fprintf(stderr, "Error: Missing interface argument.\n");
+        fprintf(stderr, "Usage: %s [options] [interface]\n", argv[0]);
         return 1;
     }
 
-    if (pcap_lookupnet(argv[1], &net, &mask, errbuf) == -1) {
-        printf("Error: Couldn't get netmask for device %s: %s\n", argv[1], errbuf);
-        net = 0;
-        mask = 0;
+    // The last argument (if any) should be the interface
+    interface = argv[optind];
+
+    // If no filter expression is set, capture all packets
+    if (strlen(filter_exp) == 0) {
+        strcpy(filter_exp, "ip");
+    } else {
+        if (strlen(filter_exp) > 0) {
+            // Remove the trailing " and " from the filter expression
+            filter_exp[strlen(filter_exp) - 5] = '\0';
+        }
     }
 
-    handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
-    if (handle == NULL) {
-        printf("Error: Couldn't open device %s: %s\n", argv[1], errbuf);
+    if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Error: Couldn't get netmask for device %s: %s\n", interface, errbuf);
         return 2;
     }
 
-    if (pcap_compile(handle, &fp, "tcp or udp or icmp or arp", 0, net) == -1) {
-        printf("Error: Couldn't parse filter %s: %s\n", "tcp or udp or icmp or arp", pcap_geterr(handle));
+    handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Error: Couldn't open device %s: %s\n", interface, errbuf);
+        return 2;
+    }
+
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Error: Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return 2;
     }
 
     if (pcap_setfilter(handle, &fp) == -1) {
-        printf("Error: Couldn't install filter %s: %s\n", "tcp or udp or icmp or arp", pcap_geterr(handle));
+        fprintf(stderr, "Error: Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return 2;
     }
 
-    printf("Listening on %s...\n", argv[1]);
+    printf("Listening on %s with filter: %s...\n", interface, filter_exp);
 
     pcap_loop(handle, -1, packet_handler, NULL);
 
@@ -55,77 +113,134 @@ int main(int argc, char *argv[])
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     struct ether_header *eth_header;
+    struct ip *ip_header;
+    struct tcphdr *tcp_header;
+    struct udphdr *udp_header;
     const u_char *payload;
     u_int ip_header_len;
     u_int tcp_header_len;
     u_int udp_header_len;
     u_int payload_len;
 
+    // Ensure the packet is at least as large as the Ethernet header
+    if (header->caplen < sizeof(struct ether_header)) {
+        fprintf(stderr, "Error: Captured packet is too small.\n");
+        return;
+    }
+
     eth_header = (struct ether_header *) packet;
-    if (ntohs(eth_header->ether_type) == ETHERTYPE_IP) {
-        struct iphdr *ip_header;
-        ip_header = (struct iphdr *) (packet + sizeof(struct ether_header));
-        ip_header_len = ip_header->ihl * 4;
 
-        if (ip_header->protocol == IPPROTO_TCP) {
-            struct tcphdr *tcp_header;
+    // Check if it's an IP packet
+    if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
+        printf("Non-IP Packet captured.\n");
+        printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
+        printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
+        printf("EtherType: %04X\n", (ntohs(eth_header->ether_type)));
+        return;
+    }
+
+    // Ensure the packet is at least as large as the IP header
+    if (header->caplen < sizeof(struct ether_header) + sizeof(struct ip)) {
+        fprintf(stderr, "Error: Captured packet is too small for IP header.\n");
+        return;
+    }
+
+    ip_header = (struct ip *) (packet + sizeof(struct ether_header));
+    ip_header_len = ip_header->ip_hl * 4;
+
+    // Ensure the packet is at least as large as the expected IP header
+    if (header->caplen < sizeof(struct ether_header) + ip_header_len) {
+        fprintf(stderr, "Error: Captured packet is too small for IP header.\n");
+        return;
+    }
+
+    // Check the protocol and handle accordingly
+    switch (ip_header->ip_p) {
+        case IPPROTO_TCP:
+            // Ensure the packet is at least as large as the TCP header
+            if (header->caplen < sizeof(struct ether_header) + ip_header_len + sizeof(struct tcphdr)) {
+                fprintf(stderr, "Error: Captured packet is too small for TCP header.\n");
+                return;
+            }
+
             tcp_header = (struct tcphdr *) (packet + sizeof(struct ether_header) + ip_header_len);
-            tcp_header_len = tcp_header->doff * 4;
+            tcp_header_len = tcp_header->th_off * 4;
 
-            payload = (u_char *) (packet + sizeof(struct ether_header) + ip_header_len + tcp_header_len);
-            payload_len = ntohs(ip_header->tot_len) - (ip_header_len + tcp_header_len);
+            // Ensure the packet is at least as large as the expected TCP header and payload
+            if (header->caplen < sizeof(struct ether_header) + ip_header_len + tcp_header_len) {
+                fprintf(stderr, "Error: Captured packet is too small for TCP header and payload.\n");
+                return;
+            }
+
+            payload = packet + sizeof(struct ether_header) + ip_header_len + tcp_header_len;
+            payload_len = ntohs(ip_header->ip_len) - ip_header_len - tcp_header_len;
 
             printf("\nTCP Packet captured.\n");
             printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
             printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
-            printf("Source IP: %s\n", inet_ntoa(*(struct in_addr*) &ip_header->saddr));
-            printf("Destination IP: %s\n", inet_ntoa(*(struct in_addr *) &ip_header->daddr));
-            printf("Source Port: %d\n", ntohs(tcp_header->source));
-            printf("Destination Port: %d\n", ntohs(tcp_header->dest));
+            printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
+            printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
+            printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
+            printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
             printf("Payload Length: %d\n", payload_len);
             printf("Payload: %.*s\n", payload_len, payload);
-        }
-        else if (ip_header->protocol == IPPROTO_UDP) {
-            struct udphdr *udp_header;
-            udp_header = (struct udphdr *) (packet + sizeof(struct ether_header) + ip_header_len);
+            break;
+
+        case IPPROTO_UDP:
+            // Ensure the packet is at least as large as the UDP header
+            if (header->caplen < sizeof(struct ether_header) + ip_header_len + sizeof(struct udphdr)) {
+                fprintf(stderr, "Error: Captured packet is too small for UDP header.\n");
+                return;
+            }
+
+            udp_header = (struct udphdr *)(packet + sizeof(struct ether_header) + ip_header_len);
             udp_header_len = sizeof(struct udphdr);
-            payload = (u_char *) (packet + sizeof(struct ether_header) + ip_header_len + udp_header_len);
-            payload_len = ntohs(ip_header->tot_len) - (ip_header_len + udp_header_len);
+
+            // Ensure the packet is at least as large as the expected UDP header and payload
+            if (header->caplen < sizeof(struct ether_header) + ip_header_len + udp_header_len) {
+                fprintf(stderr, "Error: Captured packet is too small for UDP header and payload.\n");
+                return;
+            }
+
+            payload = packet + sizeof(struct ether_header) + ip_header_len + udp_header_len;
+            payload_len = ntohs(ip_header->ip_len) - ip_header_len - udp_header_len;
+
             printf("\nUDP Packet captured.\n");
             printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
             printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
-            printf("Source IP: %s\n", inet_ntoa(*(struct in_addr *) &ip_header->saddr));
-            printf("Destination IP: %s\n", inet_ntoa(*(struct in_addr *) &ip_header->daddr));
+            printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
+            printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
             printf("Source Port: %d\n", ntohs(udp_header->uh_sport));
             printf("Destination Port: %d\n", ntohs(udp_header->uh_dport));
             printf("Payload Length: %d\n", payload_len);
             printf("Payload: %.*s\n", payload_len, payload);
-        }
-        else if (ip_header->protocol == IPPROTO_ICMP) {
-            payload = (u_char *) (packet + sizeof(struct ether_header) + ip_header_len);
-            payload_len = ntohs(ip_header->tot_len) - ip_header_len;
+            break;
+
+        case IPPROTO_ICMP:
+            // Ensure the packet is at least as large as the ICMP header
+            if (header->caplen < sizeof(struct ether_header) + ip_header_len + sizeof(struct icmphdr)) {
+                fprintf(stderr, "Error: Captured packet is too small for ICMP header.\n");
+                return;
+            }
+
+            payload = packet + sizeof(struct ether_header) + ip_header_len + sizeof(struct icmphdr);
+            payload_len = ntohs(ip_header->ip_len) - ip_header_len - sizeof(struct icmphdr);
+
             printf("\nICMP Packet captured.\n");
             printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
             printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
-            printf("Source IP: %s\n", inet_ntoa(*(struct in_addr *) &ip_header->saddr));
-            printf("Destination IP: %s\n", inet_ntoa(*(struct in_addr *) &ip_header->daddr));
+            printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
+            printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
             printf("Payload Length: %d\n", payload_len);
             printf("Payload: %.*s\n", payload_len, payload);
-        }
-    }
-    else if (ntohs(eth_header->ether_type) == ETHERTYPE_ARP) {
-        struct ether_arp *arp_header;
-        arp_header = (struct ether_arp *) (packet + sizeof(struct ether_header));
-        printf("\nARP Packet captured.\n");
-        printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
-        printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
-        printf("Sender IP: %s\n", inet_ntoa(*(struct in_addr *) arp_header->arp_spa));
-        printf("Target IP: %s\n", inet_ntoa(*(struct in_addr *) arp_header->arp_tpa));
-    }
-    else {
-        printf("\nNon-IP Packet captured.\n");
-        printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
-        printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
-        printf("EtherType: %04X\n", (ntohs(eth_header->ether_type)));
+            break;
+
+        default:
+            printf("\nUnknown Protocol Packet captured.\n");
+            printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_shost));
+            printf("Destination MAC: %s\n", ether_ntoa((struct ether_addr *) eth_header->ether_dhost));
+            printf("EtherType: %04X\n", (ntohs(eth_header->ether_type)));
+            break;
     }
 }
+
